d6208e44dd21f95d884c8062c84d1e29daf27e6f
Sarlton Ngubane <54685146+Sarlton@users.noreply.github.com>
2021-05-19 14:37:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Scroll to top button
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
